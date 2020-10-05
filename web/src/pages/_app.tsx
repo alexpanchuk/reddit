1c30dd7b2da2971154303fcdfb13fade4f4f054c
Alex Panchuk <apanchuk@gmail.com>
2020-10-05 18:32:42 +0500

    web | layout: Show NavBar on all pages (move component to _app)

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,32 @@
 import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
 import { AppProps } from "next/app";
+import React from "react";
+import { NavBar } from "../components";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import theme from "../theme";
+import { isServer } from "../utils";
+
+/**
+ * @todo Move cheking auth to context
+ */
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [{ data, fetching }] = useMeQuery({ pause: isServer() });
+  const [, logout] = useLogoutMutation();
+
   return (
     <ThemeProvider theme={theme}>
       <ColorModeProvider>
         <CSSReset />
-        <Component {...pageProps} />
+        <>
+          <NavBar
+            username={data?.me?.username}
+            isLoading={fetching}
+            logout={logout}
+          />
+
+          <Component {...pageProps} />
+        </>
       </ColorModeProvider>
     </ThemeProvider>
   );
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,37 +1,19 @@
 import { Box } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
-import { NavBar } from "../components";
-import {
-  useLogoutMutation,
-  useMeQuery,
-  usePostsQuery,
-} from "../generated/graphql";
-import { createUrqlClient, isServer } from "../utils";
-
-/**
- * @todo Move cheking auth to context
- */
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils";
 
 const Index = () => {
-  const [{ data, fetching }] = useMeQuery({ pause: isServer() });
-  const [, logout] = useLogoutMutation();
   const [{ data: posts }] = usePostsQuery();
 
   return (
-    <>
-      <NavBar
-        username={data?.me?.username}
-        isLoading={fetching}
-        logout={logout}
-      />
-      <Box p={4}>
-        {!posts ? (
-          <div>loading...</div>
-        ) : (
-          posts.posts.map(({ title, id }) => <div key={id}>{title}</div>)
-        )}
-      </Box>
-    </>
+    <Box p={4}>
+      {!posts ? (
+        <div>loading...</div>
+      ) : (
+        posts.posts.map(({ title, id }) => <div key={id}>{title}</div>)
+      )}
+    </Box>
   );
 };
 
